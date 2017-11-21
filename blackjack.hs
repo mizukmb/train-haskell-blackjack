8ff3158dae623af8321d8afd2145177f01bee0f4
mizukmb <mizukmb6@gmail.com>
2017-11-21 23:47:19 +0900

    Add function sumHand

--- blackjack.hs
+++ blackjack.hs
@@ -2,3 +2,6 @@
 --   A | N2 | N3 | N4 | N5 | N6 | N7 | N8 | N9 | N10 | J | Q | K deriving (Eq, Show)
 data Card =
   A | N Int | J | Q | K deriving (Eq, Show)
+
+sumHand :: [Card] -> Int
+sumHand cards = undefind
