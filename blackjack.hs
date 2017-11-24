337eccc00c44cd1267840f450e0e90763242baaa
mizukmb <mizukmb6@gmail.com>
2017-11-25 01:12:10 +0900

    Add sumHand function

--- blackjack.hs
+++ blackjack.hs
@@ -4,9 +4,25 @@ data Card =
   A | N Int | J | Q | K deriving (Eq, Show)
 
 sumHand :: [Card] -> Int
-sumHand cards = undefined
+sumHand cards =
+  let possiblePoints  = map toPoint cards
+      scoreCandidates = foldl plusEach [0] possiblePoints
+      noBust          = filter (<= 21) scoreCandidates
+  in 
+    if null noBust
+      then head scoreCandidates
+      else maximum noBust
 
 toPoint :: Card -> [Int]
 toPoint A = [1, 11]
 toPoint (N n) = [n]
 toPoint _ = [10]
+
+plusEach :: [Int] -> [Int] -> [Int]
+plusEach list1 list2 =
+  concatMap(\element1 ->
+    map(\element2 ->
+      element1 + element2
+    ) list2
+  ) list1
+
