3bac63a9b9d7ba26e625fc6b9854ebac61bd650f
mizukmb <mizukmb6@gmail.com>
2017-11-25 16:04:23 +0900

    Done
    - https://employment.en-japan.com/engineerhub/entry/2017/10/03/110000

--- blackjack.hs
+++ blackjack.hs
@@ -1,3 +1,7 @@
+import Data.List.NonEmpty (NonEmpty((:|)))
+import qualified Data.List.NonEmpty as NonEmpty
+import Data.Semigroup (sconcat)
+
 -- data Card =
 --   A | N2 | N3 | N4 | N5 | N6 | N7 | N8 | N9 | N10 | J | Q | K deriving (Eq, Show)
 data Card =
@@ -6,23 +10,24 @@ data Card =
 sumHand :: [Card] -> Int
 sumHand cards =
   let possiblePoints  = map toPoint cards
-      scoreCandidates = foldl plusEach [0] possiblePoints
-      noBust          = filter (<= 21) scoreCandidates
+      scoreCandidates = foldl plusEach (0 :| []) possiblePoints
+      noBust          = NonEmpty.filter (<= 21) scoreCandidates
   in 
     if null noBust
-      then head scoreCandidates
+      then NonEmpty.head scoreCandidates
       else maximum noBust
 
-toPoint :: Card -> [Int]
-toPoint A = [1, 11]
-toPoint (N n) = [n]
-toPoint _ = [10]
+toPoint :: Card -> NonEmpty Int
+toPoint A = 1 :| [11]
+toPoint (N n) = n :| []
+toPoint _ = 10 :| []
 
-plusEach :: [Int] -> [Int] -> [Int]
+plusEach :: NonEmpty Int -> NonEmpty Int -> NonEmpty Int
 plusEach list1 list2 =
-  concatMap(\element1 ->
-    map(\element2 ->
-      element1 + element2
-    ) list2
-  ) list1
-
+  sconcat(
+    NonEmpty.map(\element2 ->
+      NonEmpty.map(\element1 ->
+        element1 + element2
+      ) list2
+    ) list1
+  )
